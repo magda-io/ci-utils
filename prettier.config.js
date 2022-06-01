3cca3aa9cd3a6ba05bce51ea627e08d0858fa0f1
Jacky Jiang <t83714@gmail.com>
2022-06-01 17:38:10 +1000

    add prettier config & reformat code

---
+++ prettier.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: false,
+    printWidth: 80,
+    trailingComma: "none",
+    useTabs: false,
+    overrides: [
+        {
+            files: ["**/package.json", "lerna.json"],
+            options: {
+                tabWidth: 2
+            }
+        },
+        {
+            files: [
+                "**/*.yml",
+                "**/*.yaml",
+                "**/.*.yaml",
+                "**/.*.yml",
+                "**/*.yaml"
+            ],
+            options: {
+                tabWidth: 2
+            }
+        },
+        {
+            files: ["**/*.md"],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
